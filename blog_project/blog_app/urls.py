d98d855a4ac698cdbda4b67a38ba20c6a54ec3e6
Bibek Shah <124855267+Bibekshah123@users.noreply.github.com>
2025-04-29 15:48:58 +0545

    .

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -12,11 +12,4 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    
-    
-    # path('post/new/', post_create , name='post_create'),
-    # path('post/<int:pk>/edit/', post_update, name='post_update'),
-    # path('post/<int:pk>/delete/', post_delete, name='post_delete'),
-    # path('post/<int:pk>/comment/', add_comment, name='add_comment'),
-
 ]
